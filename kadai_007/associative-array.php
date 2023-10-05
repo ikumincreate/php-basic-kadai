60d1556113b54c7d92ecb4bcfa557f71810110b1
ikumincreate <141818014+ikumincreate@users.noreply.github.com>
2023-10-05 17:48:51 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charest="UTF-8">
+  <title>PHP基礎 課題005</title>
+</head>
+
+<body>
+  <p>
+  <?php
+    // 連想配列に値を代入する
+    $personal_date = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($personal_date);
+  ?>
+  </p>
+</body>
